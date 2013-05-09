66c1d8c63beb58d8e27667aa0f52337d36e802ec
Talustus <talustus@dream-irc.de>
2013-05-09 11:01:27 +0200

    TWRP 2.5.0.0 GT-I9505 Ramfs Import

---
+++ sbin/postrecoveryboot.sh
@@ -0,0 +1,12 @@
+#!/sbin/sh
+
+# The partition to use is named "PARAM" and can be found by dumping the PIT.
+
+# 0: charge
+# 1: direct
+# 2: recovery_enter
+# 3: recovery_end
+# 4: download_end
+
+sleep 3
+echo 3 > /dev/block/platform/msm_sdcc.1/by-name/param
