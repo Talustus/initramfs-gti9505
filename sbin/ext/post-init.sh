9c73477ebe620a1d66bf48306c9cad168dafd2b4
Talustus <talustus@dream-irc.de>
2013-05-25 14:11:06 +0200

    Exfat-Fuse: Add exfat support back to kernel via exfat-fuse   * add exfat binaries   * update post init to check for exfat sd cards ...

--- sbin/ext/post-init.sh
+++ sbin/ext/post-init.sh
@@ -24,6 +24,19 @@ exec >>/data/.dream/post-init.log 2>&1
 
 echo "Running Post-Init Script"
 
+## Testing: Check for ExFat SD Card
+#
+SDTYPE=`blkid /dev/block/mmcblk1p1  | awk '{ print $3 }' | sed -e 's|TYPE=||g' -e 's|\"||g'`
+
+if [ ${SDTYPE} == "exfat" ];
+then
+  echo "ExFat-Debug: SD-Card is type ExFAT"
+  echo "ExFat-Debug: trying to mount via fuse"
+  mount.exfat-fuse /dev/block/mmcblk1p1 /storage/extSdCard
+else
+  echo "ExFat-Debug: SD-Card is type: ${SDTYPE}"
+fi
+
 # Remount FileSys RO
 /sbin/busybox mount -t rootfs -o remount,ro rootfs
 
